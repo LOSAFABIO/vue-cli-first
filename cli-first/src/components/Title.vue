e92ab6085173c1dcdd034b53990b1027aaca67fb
LOSAFABIO <fabio.losa95@gmail.com>
2022-01-26 15:48:19 +0100

    Non funziona, errore in App.vue

---
+++ cli-first/src/components/Title.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Title",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+  font-size: 50px;
+  color: green;
+}
+</style>
